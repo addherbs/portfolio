9c8d2dd18088207fa8f1f13e7f704275897744ef
Aditya Pandey <addherbs@gmail.com>
2020-12-06 04:47:04 -0600

    Updated Resume link, About Me info and Added some Mouser Electronics details

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 //Set Port
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 5000));
 
 //Listen to port
 app.listen( app.get('port'), function () {
